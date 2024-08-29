8957f9022126a4b48f7623206a5d86fdbe55977b
Vimal Vinod V <134259948+VimalVinod@users.noreply.github.com>
2024-08-29 11:52:05 +0530

    Update ShapePerimeter.java

--- S3C67/Java/ShapePerimeter.java
+++ S3C67/Java/ShapePerimeter.java
@@ -1,33 +1,54 @@
+import java.util.*;
+
 class ShapePerimeter {
-    private double radius;
-    private double length;
-    private double width;
 
-    
-    public ShapePerimeter(double radius) {
-        this.radius = radius;
+    // Method to calculate perimeter of a circle
+    public static void calculateCirclePerimeter(double radius) {
+        double pi = 3.14;
+        double perimeter = 2 * pi * radius;
+        System.out.println("Perimeter of the circle: " + perimeter);
     }
 
-    
-    public ShapePerimeter(double length, double width) {
-        this.length = length;
-        this.width = width;
-    }
-
-    public double getPerimeter() {
-        double pi = 3.141592653589793;
-        return 2 * pi * radius;
-    }
-
-    public double getPerimeterRectangle() {
-        return 2 * (length + width);
+    // Method to calculate perimeter of a rectangle
+    public static void calculateRectanglePerimeter(int length, int width) {
+        double perimeter = 2 * (length + width);
+        System.out.println("Perimeter of the rectangle: " + perimeter);
     }
 
     public static void main(String[] args) {
-        ShapePerimeter circle = new ShapePerimeter(5.0);
-        ShapePerimeter rectangle = new ShapePerimeter(4.0, 6.0);
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
 
-        System.out.println("Perimeter of the circle: " + circle.getPerimeter());
-        System.out.println("Perimeter of the rectangle: " + rectangle.getPerimeterRectangle());
+        while (choice != 3) {
+            System.out.println("Select shape to calculate perimeter:");
+            System.out.println("1. Circle");
+            System.out.println("2. Rectangle");
+            System.out.println("3. Exit");
+            
+            System.out.print("Enter your choice (1/2/3): ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the radius of the circle: ");
+                    double radius = scanner.nextDouble();
+                    calculateCirclePerimeter(radius);
+                    break;
+                case 2:
+                    System.out.print("Enter the length of the rectangle: ");
+                    int length = scanner.nextInt();
+                    System.out.print("Enter the width of the rectangle: ");
+                    int width = scanner.nextInt();
+                    calculateRectanglePerimeter(length, width);
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        }
+        scanner.close();
     }
 }
